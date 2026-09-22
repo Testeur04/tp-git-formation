12641113af5d28c2067b5205f7d1b85fc4145bf1
U1 <abdoulfathounikiema24@gmail.com>
2026-09-22 19:45:16 +0000

    feature: ajoute du formulaire de login

---
+++ login.js
@@ -0,0 +1,10 @@
+// Formulaire de connexion - fonctionnalité A (U2)
+const NOM_UTILISATEUR = "u2";
+
+function login() {
+  // Instructions de connexion
+  console.log("Connexion de " + NOM_UTILISATEUR);
+  return true;
+}
+
+module.exports = { login };
